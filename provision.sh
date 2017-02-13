c64b24ba47a35b0992af2f44f8cebea53c803be2
Mark deVilliers <markdevilliers@gmail.com>
2017-02-13 09:58:48 +0000

    refresh deps

--- provision.sh
+++ provision.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-export DEBIAN_FRONTEND=noninteractive 
+export DEBIAN_FRONTEND=noninteractive
 
 wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
 sudo dpkg -i erlang-solutions_1.0_all.deb
 
 apt-get update > /dev/null
 
-apt-get -y install make zip xsltproc esl-erlang git
-
-
-cd /vagrant
-
-git clone https://github.com/rabbitmq/rabbitmq-public-umbrella.git
-
-cd rabbitmq-public-umbrella
-
-make co
+apt-get -y install make zip xsltproc esl-erlang git xmlto elixir
 
